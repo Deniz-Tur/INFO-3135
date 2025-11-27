f2d1b241260b663af15272193527a69d5de60c7f
Nikita1420-g <nikita.gupta1@student.kpu.ca>
2025-11-27 02:30:12 -0500

    modified files

--- my_reservations.php
+++ my_reservations.php
@@ -9,8 +9,8 @@ if (!isset($_SESSION['user_id'])) {
 }
 
 $activeTab = 'my_reservations';
-require 'includes/db.php';
-include 'includes/header.php';
+require 'db.php';
+include 'header.php';
 
 $userId = $_SESSION['user_id'];
 $successMessage = '';
@@ -201,5 +201,5 @@ $pastReservations = $pastStmt->fetchAll();
 </div>
 
 <?php
-include 'includes/footer.php';
+include 'footer.php';
 ?>
